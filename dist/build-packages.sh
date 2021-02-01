c5d8d784076f510a47a719e63922f3920174f590
Andreas Lemberger <l3mde@users.noreply.github.com>
2021-02-01 15:30:51 +0100

    Update build-packages.sh

--- dist/build-packages.sh
+++ dist/build-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p ~/.cache/paru
 for i in $@; do
-    paru --noconfirm -Syu $i
+    paru --useask --noconfirm -Syu $i
 done
 find ~/.cache/paru -name '*.pkg.tar.zst' -exec mv '{}' /packages \;
